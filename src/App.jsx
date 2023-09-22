97a7e3d4857b38041cca6e91b2ced202f5175b98
Namrata <namratabose32@gmail.com>
2023-09-22 18:56:36 +0530

    Highlight

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
       <main className='flex flex-col gap-16 relative'>
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists/>
-        {/* <FlexContent endpoint={highlight} ifExists /> */}
+        <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={toprateslaes}/>
-        {/* <FlexContent endpoint={sneaker}/> */}
+        <FlexContent endpoint={sneaker}/>
       </main>
     </>
   )
