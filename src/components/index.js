22422c081c8d2f1491e96f6298de212940a1b885
Namrata <namratabose32@gmail.com>
2023-09-22 19:30:45 +0530

    Stories

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Hero} from './Hero.jsx';
 export {default as Sales} from './Sales';
 export {default as FlexContent} from './FlexContent.jsx';
+export {default as Stories} from './Stories.jsx'
